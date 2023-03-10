3a66a67c6ba802369c0067290b21da7d9236b251
Alexandra Stancheva <66909315+allks12@users.noreply.github.com>
2023-03-10 12:51:57 +0200

    Add card template.

--- app/src/main/java/com/example/besafeapp/ui/LearningFragment.kt
+++ app/src/main/java/com/example/besafeapp/ui/LearningFragment.kt
@@ -1,20 +1,43 @@
 package com.example.besafeapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.besafeapp.R
+import com.example.besafeapp.data.Datasource
+import com.example.besafeapp.databinding.FragmentLearningBinding
+import com.example.besafeapp.databinding.FragmentSafetyListBinding
+import org.json.JSONObject
 
 
 class LearningFragment : Fragment() {
+
+    private var _binding: FragmentLearningBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_learning, container, false)
+        _binding = FragmentLearningBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.learningListRV
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = LearningAdapter(Datasource().loadTopics())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
